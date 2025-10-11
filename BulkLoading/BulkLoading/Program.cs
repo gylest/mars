982b2916fdbcd6a758b13189485072c2086e8068
Tony Gyles <tonygyles@btinternet.com>
2025-10-11 09:39:58 +0100

    Use dependency injection to pass configuration data to forms.

--- BulkLoading/BulkLoading/Program.cs
+++ BulkLoading/BulkLoading/Program.cs
@@ -1,10 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace BulkLoading
+﻿namespace BulkLoading
 {
     static class Program
     {
@@ -14,10 +8,31 @@ namespace BulkLoading
         [STAThread]
         static void Main()
         {
+            // Build configuration
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            // Setup Dependency Injection
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(configuration);
+
+            // Bind AppSettings section to strongly typed class
+            var appSettings = new AppSettings();
+            configuration.GetSection("AppSettings").Bind(appSettings);
+            services.AddSingleton(appSettings);
+
+            services.AddTransient<MainForm>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Start the application
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+
+            Application.Run(serviceProvider.GetRequiredService<MainForm>());
         }
     }
 }
