05967cc60638c35a17fab6c88bc38c5367554991
Tony Gyles <tonygyles@btinternet.com>
2025-10-19 15:53:57 +0100

    Modernize using statements

--- EvaluateTestFrameworks/CodeLibrary/Json.cs
+++ EvaluateTestFrameworks/CodeLibrary/Json.cs
@@ -1,29 +1,24 @@
-﻿using Newtonsoft.Json.Linq;
-using System;
-using System.Diagnostics;
+﻿namespace CodeLibrary;
 
-namespace CodeLibrary
+public static class JsonHelper
 {
-    public static class JsonHelper
+    public static string GetCompany(string jsonString)
     {
-        public static string GetCompany(string jsonString)
-        {
-            //
-            // Dynamic Deserialization - Simple JSON to Values
-            //
+        //
+        // Dynamic Deserialization - Simple JSON to Values
+        //
 
-            dynamic json = JValue.Parse(jsonString);
+        dynamic json = JValue.Parse(jsonString);
 
-            // json values
-            string name = json.Name;
-            string company = json.Company;
-            DateTime entered = json.Entered;
+        // json values
+        string name = json.Name;
+        string company = json.Company;
+        DateTime entered = json.Entered;
 
-            // Show parse result
-            Debug.WriteLine($"JSON Parse Results: Name = {name}, Company = {company}, Entered = {entered}");
+        // Show parse result
+        Debug.WriteLine($"JSON Parse Results: Name = {name}, Company = {company}, Entered = {entered}");
 
-            // return 
-            return company;
-        }
+        // return 
+        return company;
     }
 }
--- EvaluateTestFrameworks/CodeLibrary/Math.cs
+++ EvaluateTestFrameworks/CodeLibrary/Math.cs
@@ -1,26 +1,25 @@
-﻿namespace CodeLibrary
+﻿namespace CodeLibrary;
+
+public static class Math
 {
-    public static class Math
+    public static int AddTwoNumbers(int a, int b)
     {
-        public static int AddTwoNumbers(int a, int b)
+        return a + b;
+    }
+
+    public static int SubtractTwoNumbers(int a, int b)
+    {
+        return a - b;
+    }
+
+    public static int MultiplyTwoNumbers(int a, int b)
+    {
+        // Add a condition that should not be hit, to highlight for code coverage
+        if (a < (-10000))
         {
-            return a + b;
+            b = -100;
         }
 
-        public static int SubtractTwoNumbers(int a, int b)
-        {
-            return a - b;
-        }
-
-        public static int MultiplyTwoNumbers(int a, int b)
-        {
-            // Add a condition that should not be hit, to highlight for code coverage
-            if (a < (-10000))
-            {
-                b = -100;
-            }
-
-            return a * b;
-        }
+        return a * b;
     }
 }
